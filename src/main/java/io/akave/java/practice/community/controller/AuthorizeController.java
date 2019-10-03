407754e5b9dffe72d8973f2d76722d7c638594a6
akave <akave@gmail.com>
2019-10-03 21:31:57 +0800

    store user data into h2db

--- src/main/java/io/akave/java/practice/community/controller/AuthorizeController.java
+++ src/main/java/io/akave/java/practice/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package io.akave.java.practice.community.controller;
 
 import io.akave.java.practice.community.dto.AccessTokenDTO;
 import io.akave.java.practice.community.dto.GithubUser;
+import io.akave.java.practice.community.mapper.UserMapper;
+import io.akave.java.practice.community.model.User;
 import io.akave.java.practice.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,17 +14,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+
     @Value("${github.client.id}")
     private String clientId;
+
     @Value("${github.client.secret}")
     private String clientSecret;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
     public AuthorizeController(GithubProvider githubProvider) {
         this.githubProvider = githubProvider;
     }
@@ -41,6 +50,13 @@ public class AuthorizeController {
         String token = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getGithubUser(token);
         if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             httpServletRequest.getSession().setAttribute("user", githubUser);
             return "redirect:/";
         } else {
