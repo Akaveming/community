191af929a91aa9d024690de36b785373e53fb219
akave <akave@gmail.com>
2019-10-03 14:24:25 +0800

    add index and bootstrap

---
+++ src/main/java/io/akave/java/practice/community/controller/Index.java
@@ -0,0 +1,12 @@
+package io.akave.java.practice.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Index {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
